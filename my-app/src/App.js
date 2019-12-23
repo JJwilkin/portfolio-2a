f32bf7c21634e1a3871114b024b9aa8061d289d3
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2019-12-22 23:30:13 -0500

    fix versioning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-      <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    
-  </Navbar.Collapse>
-</Navbar>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
