8303b7bdbf39d1e815af74b0b02dfcf61bf958d9
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2019-12-22 22:03:21 -0500

    first

---
+++ build/precache-manifest.6da82fd54995d79bae9d5e2362d4e99a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ceef4815e5259bbc954ab4ca9051b31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a1fdb52a611ea6d88b8",
+    "url": "/static/css/main.caf8ed15.chunk.css"
+  },
+  {
+    "revision": "7ce84e4e01d99b192669",
+    "url": "/static/js/2.123098c9.chunk.js"
+  },
+  {
+    "revision": "5a1fdb52a611ea6d88b8",
+    "url": "/static/js/main.3f52c9bb.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
