b9689c690c8774c16d9ba09fef6527e0a107654b
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2019-12-22 22:47:31 -0500

    update webpack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    
+  </Navbar.Collapse>
+</Navbar>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
