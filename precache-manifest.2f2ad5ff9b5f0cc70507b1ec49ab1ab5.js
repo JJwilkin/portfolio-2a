28f2f3b9927440ae8222e2725c3fc9582a4ad442
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2020-01-13 14:54:27 -0500

    Updates

---
+++ precache-manifest.2f2ad5ff9b5f0cc70507b1ec49ab1ab5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24b4633d60528eaf80efd1c00acb23b",
+    "url": "/jjwilkin/index.html"
+  },
+  {
+    "revision": "967d8e5765253fd39174",
+    "url": "/jjwilkin/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5c368294254f9b2944cb",
+    "url": "/jjwilkin/static/css/main.b8fac6ea.chunk.css"
+  },
+  {
+    "revision": "967d8e5765253fd39174",
+    "url": "/jjwilkin/static/js/2.f51fb6c0.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "/jjwilkin/static/js/2.f51fb6c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c368294254f9b2944cb",
+    "url": "/jjwilkin/static/js/main.6202ada8.chunk.js"
+  },
+  {
+    "revision": "3981823dcf7b5d51f645",
+    "url": "/jjwilkin/static/js/runtime-main.ebb2a96a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jjwilkin/static/media/logo.5d5d9eef.svg"
+  }
+]);
