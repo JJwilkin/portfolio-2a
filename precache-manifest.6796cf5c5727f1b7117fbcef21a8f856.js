caf87478bbc8b466bdf1d265ecb2d55c20fd0e8e
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2020-01-13 19:39:52 -0500

    Updates

---
+++ precache-manifest.6796cf5c5727f1b7117fbcef21a8f856.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca97bbec68a4d3ba8207048a88caab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "967d8e5765253fd39174",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "edc1624811f0f447cc63",
+    "url": "/static/css/main.b8fac6ea.chunk.css"
+  },
+  {
+    "revision": "967d8e5765253fd39174",
+    "url": "/static/js/2.f51fb6c0.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "/static/js/2.f51fb6c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edc1624811f0f447cc63",
+    "url": "/static/js/main.44b69dd4.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
