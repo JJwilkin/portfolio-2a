f61eab4beaeb7a608016dd8f1dd6396d097c392e
Joshua Wilkinson <joshuawilkinson@Joshuas-MacBook-Air.local>
2020-01-14 02:34:14 -0500

    Updates

---
+++ precache-manifest.47ce5e6523ea746c8f5c45d69912e499.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaff75053082a07c9dc021198856ce3",
+    "url": "joshuawilkinson.ca/index.html"
+  },
+  {
+    "revision": "967d8e5765253fd39174",
+    "url": "joshuawilkinson.ca/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bd539e11740f3ff1afb2",
+    "url": "joshuawilkinson.ca/static/css/main.b8fac6ea.chunk.css"
+  },
+  {
+    "revision": "967d8e5765253fd39174",
+    "url": "joshuawilkinson.ca/static/js/2.f51fb6c0.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "joshuawilkinson.ca/static/js/2.f51fb6c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd539e11740f3ff1afb2",
+    "url": "joshuawilkinson.ca/static/js/main.bdb3bac9.chunk.js"
+  },
+  {
+    "revision": "4d4bf1bae0d63e491f8f",
+    "url": "joshuawilkinson.ca/static/js/runtime-main.0836e711.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "joshuawilkinson.ca/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-2a/precache-manifest.ca7da1afa831c7f28a4164046f80c96b.js"
+  "joshuawilkinson.ca/precache-manifest.47ce5e6523ea746c8f5c45d69912e499.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-2a/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("joshuawilkinson.ca/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
