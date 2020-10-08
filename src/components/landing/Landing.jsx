d95a104166e80e306014bffbdc448f515304e53b
Joshua Wilkinson <joshjeffreywilkinson@gmail.com>
2020-10-07 21:29:23 -0400

    update landing text

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -9,7 +9,7 @@ export default function Landing ()  {
             <h1 >I'm Josh.</h1>
             </div>
             
-            <div class="text" > <h2 >Full stack developer seeking <strong>Summer 2020</strong> software internships!</h2></div>
+            <div class="text" > <h2 >Experienced developer seeking <strong>Winter 2021</strong> software internships!</h2></div>
         </div>
         
         <img src='./img/landing.svg' alt='landing'></img>
